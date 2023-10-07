71b7335f678b4ebc7ee3e37f3fc983bfcabf6767
DarkPred <26512194+darkpred@users.noreply.github.com>
2023-10-07 02:24:20 +0200

    Add missing tree registry on forge

--- forge/src/main/java/com/fossil/fossil/world/feature/forge/ModFeaturesImpl.java
+++ forge/src/main/java/com/fossil/fossil/world/feature/forge/ModFeaturesImpl.java
@@ -12,6 +12,10 @@ import static com.fossil.fossil.world.feature.ModFeatures.*;
 public class ModFeaturesImpl {
     public static void register() {
         ASH_DISK.feature().setRegistryName(ASH_DISK.location());
+        CALAMITES_TREE.feature().setRegistryName(CALAMITES_TREE.location());
+        CORDAITES_TREE.feature().setRegistryName(CORDAITES_TREE.location());
+        PALM_TREE.feature().setRegistryName(PALM_TREE.location());
+        SIGILLARIA_TREE.feature().setRegistryName(SIGILLARIA_TREE.location());
         MOAI_STATUE.feature().setRegistryName(MOAI_STATUE.location());
         VOLCANO_CONE.feature().setRegistryName(VOLCANO_CONE.location());
     }
@@ -20,6 +24,10 @@ public class ModFeaturesImpl {
     public static void registerFeatures(RegistryEvent.Register<Feature<?>> event) {
         IForgeRegistry<Feature<?>> registry = event.getRegistry();
         registry.register(ASH_DISK.feature());
+        registry.register(CALAMITES_TREE.feature());
+        registry.register(CORDAITES_TREE.feature());
+        registry.register(PALM_TREE.feature());
+        registry.register(SIGILLARIA_TREE.feature());
         registry.register(MOAI_STATUE.feature());
         registry.register(VOLCANO_CONE.feature());
     }
