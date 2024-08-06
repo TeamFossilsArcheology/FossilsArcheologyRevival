94655104253402b9b43342972880f1338062cd96
DarkPred <26512194+DarkPred@users.noreply.github.com>
2024-08-06 23:15:28 +0200

    Update fossil block loot table

--- forge/src/main/java/com/fossil/fossil/forge/data/loot/ModBlockLootTables.java
+++ forge/src/main/java/com/fossil/fossil/forge/data/loot/ModBlockLootTables.java
@@ -4,8 +4,6 @@ import com.fossil.fossil.block.PrehistoricPlantInfo;
 import com.fossil.fossil.block.custom_blocks.AmphoraVaseBlock;
 import com.fossil.fossil.block.custom_blocks.FourTallFlowerBlock;
 import com.fossil.fossil.block.custom_blocks.TallFlowerBlock;
-import com.fossil.fossil.enchantment.ModEnchantments;
-import com.fossil.fossil.item.ModItems;
 import com.fossil.fossil.tags.ModItemTags;
 import net.minecraft.advancements.critereon.EnchantmentPredicate;
 import net.minecraft.advancements.critereon.ItemPredicate;
@@ -31,6 +29,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static com.fossil.fossil.block.ModBlocks.*;
+import static com.fossil.fossil.enchantment.ModEnchantments.ARCHEOLOGY;
+import static com.fossil.fossil.enchantment.ModEnchantments.PALEONTOLOGY;
+import static com.fossil.fossil.item.ModItems.*;
 
 public class ModBlockLootTables extends BlockLoot {
 
@@ -38,13 +39,20 @@ public class ModBlockLootTables extends BlockLoot {
             ANU_STATUE.get(), ANUBITE_STATUE.get(), ANCIENT_CHEST.get(), ANU_BARRIER.get(),
             MUTANT_TREE_TUMOR.get());
     private final List<Block> tableDone = new ArrayList<>();
+    private int scarab;
+    private int broken;
+    private int skullBlock;
+    private int bioFossil;
+    private int plantFossil;
+    private int relic;
+    private int bone;
 
     @Override
     protected void addTables() {
         AMBER_ORE.ifPresent(block -> addCustom(block, createOreDrop(block, Item.byBlock(AMBER_CHUNK.get()))));
         ICED_DIRT.ifPresent(block -> addCustom(block, createSilkTouchOnlyTable(block)));
-        PERMAFROST_BLOCK.ifPresent(block -> addCustom(block, multiple(20, ModItems.FERN_SEED_FOSSIL.get(),
-                SKULL_BLOCK.get(), ModItems.FROZEN_MEAT.get(), Items.BONE, Items.BOOK)));
+        PERMAFROST_BLOCK.ifPresent(block -> addCustom(block, multiple(20, FERN_SEED_FOSSIL.get(),
+                SKULL_BLOCK.get(), FROZEN_MEAT.get(), Items.BONE, Items.BOOK)));
 
         SLIME_TRAIL.ifPresent(block -> addCustom(block, randomItem(Items.SLIME_BALL, 0.33f)));
 
@@ -68,13 +76,22 @@ public class ModBlockLootTables extends BlockLoot {
             addCustom(flower, LootTable.lootTable().withPool(LootPool.lootPool().setRolls(ConstantValue.exactly(1)).add(condition)
                     .when(ExplosionCondition.survivesExplosion())).setParamSet(LootContextParamSets.BLOCK));
         }
-        var archList1 = group(6, 3, 7, 37, 275, 475, 75, 200, 4, 3, 3, 3, 3, 3, 3).when(enchant(ModEnchantments.ARCHEOLOGY.get(), 1));
-        var archList2 = group(11, 1, 9, 37, 275, 750, 0, 0, 0, 2, 3, 3, 3, 3, 3).when(enchant(ModEnchantments.ARCHEOLOGY.get(), 2));
-        var archList3 = group(16, 0, 10, 37, 275, 750, 0, 0, 0, 0, 0, 3, 3, 3, 3).when(enchant(ModEnchantments.ARCHEOLOGY.get(), 3));
-        var paleList1 = group(6, 3, 7, 75, 375, 25, 550, 0, 28, 6, 6, 6, 6, 6, 6).when(enchant(ModEnchantments.PALEONTOLOGY.get(), 1));
-        var paleList2 = group(11, 1, 9, 112, 475, 0, 400, 0, 48, 9, 9, 9, 9, 9, 9).when(enchant(ModEnchantments.PALEONTOLOGY.get(), 2));
-        var paleList3 = group(16, 0, 10, 150, 575, 0, 225, 0, 67, 12, 12, 12, 12, 12, 12).when(enchant(ModEnchantments.PALEONTOLOGY.get(), 3));
-        var defauList = group(1, 5, 5, 37, 275, 200, 350, 200, 9, 3, 3, 3, 3, 3, 3);
+        //lol this is so bad but I like it
+        var defauList = scarab(1).broken(5).skullBlock(34).bio(275).plant(110).relic(200).bone(440)
+                .group(6, 6, 3, 3, 3, 3, 3, 3);
+        var archList1 = scarab(6).broken(9).skullBlock(64).bio(225).plant(85).relic(475).bone(200)
+                .group(3, 3, 3, 3, 3, 3, 3, 3).when(enchant(ARCHEOLOGY.get(), 1));
+        var archList2 = scarab(11).broken(11).skullBlock(110).bio(132).plant(50).relic(715).bone(36).
+                group(3, 3, 3, 3, 3, 3, 3, 3).when(enchant(ARCHEOLOGY.get(), 2));
+        var archList3 = scarab(16).broken(13).skullBlock(144).bio(50).plant(20).relic(820).bone(0).
+                group(3, 3, 3, 3, 3, 3, 3, 3).when(enchant(ARCHEOLOGY.get(), 3));
+
+        var paleList1 = scarab(1).broken(5).skullBlock(55).bio(375).plant(145).relic(20).bone(440)
+                .group(9, 9, 6, 6, 6, 6, 6, 6).when(enchant(PALEONTOLOGY.get(), 1));
+        var paleList2 = scarab(1).broken(5).skullBlock(30).bio(470).plant(175).relic(0).bone(326)
+                .group(13, 13, 10, 10, 12, 10, 10, 10).when(enchant(PALEONTOLOGY.get(), 2));
+        var paleList3 = scarab(1).broken(5).skullBlock(36).bio(510).plant(205).relic(0).bone(210)
+                .group(18, 18, 15, 15, 17, 15, 15, 15).when(enchant(PALEONTOLOGY.get(), 3));
         addCustom(FOSSIL.get(), LootTable.lootTable().withPool(LootPool.lootPool().setRolls(ConstantValue.exactly(1))
                         .add(AlternativesEntry.alternatives(archList1, archList2, archList3, paleList1, paleList2, paleList3, defauList)))
                 .setParamSet(LootContextParamSets.BLOCK));
@@ -118,19 +135,54 @@ public class ModBlockLootTables extends BlockLoot {
         return MatchTool.toolMatches(ItemPredicate.Builder.item().hasEnchantment(new EnchantmentPredicate(enchantment, MinMaxBounds.Ints.exactly(value))));
     }
 
+    private ModBlockLootTables scarab(int weight) {
+        scarab = weight;
+        return this;
+    }
+
+    private ModBlockLootTables broken(int weight) {
+        broken = weight;
+        return this;
+    }
+
+    private ModBlockLootTables skullBlock(int weight) {
+        skullBlock = weight;
+        return this;
+    }
+
+    private ModBlockLootTables relic(int weight) {
+        relic = weight;
+        return this;
+    }
+
+    private ModBlockLootTables bio(int weight) {
+        bioFossil = weight;
+        return this;
+    }
+
+    private ModBlockLootTables plant(int weight) {
+        plantFossil = weight;
+        return this;
+    }
+
+    private ModBlockLootTables bone(int weight) {
+        bone = weight;
+        return this;
+    }
+
     private EntryGroup.Builder group(int... weights) {
-        return EntryGroup.list(lootItem(ModItems.SCARAB_GEM.get(), weights[0]), lootItem(ModItems.BROKEN_SWORD.get(), weights[1]),
-                lootItem(ModItems.BROKEN_HELMET.get(), weights[2]), lootItem(SKULL_BLOCK.get(), weights[3]),
-                lootItem(ModItems.BIO_FOSSIL.get(), weights[4]), lootItem(ModItems.RELIC_SCRAP.get(), weights[5]),
-                lootItem(Items.BONE, weights[6]), lootItem(ModItems.PlANT_FOSSIL.get(), weights[7]),
-                DynamicLoot.dynamicEntry(ModItemTags.LEG_BONES.location()).setWeight(weights[8]),
-                DynamicLoot.dynamicEntry(ModItemTags.ARM_BONES.location()).setWeight(weights[9]),
-                DynamicLoot.dynamicEntry(ModItemTags.FOOT_BONES.location()).setWeight(weights[10]),
-                DynamicLoot.dynamicEntry(ModItemTags.SKULL_BONES.location()).setWeight(weights[11]),
-                DynamicLoot.dynamicEntry(ModItemTags.RIBCAGE_BONES.location()).setWeight(weights[12]),
-                DynamicLoot.dynamicEntry(ModItemTags.VERTEBRAE_BONES.location()).setWeight(weights[13]),
-                DynamicLoot.dynamicEntry(ModItemTags.UNIQUE_BONES.location()).setWeight(weights[14]));
-        //TODO: Tail bones and fix this entire thing
+        return EntryGroup.list(lootItem(SCARAB_GEM.get(), scarab), lootItem(BROKEN_SWORD.get(), broken),
+                lootItem(BROKEN_HELMET.get(), broken), lootItem(SKULL_BLOCK.get(), skullBlock),
+                lootItem(BIO_FOSSIL.get(), bioFossil), lootItem(RELIC_SCRAP.get(), relic),
+                lootItem(Items.BONE, bone), lootItem(PlANT_FOSSIL.get(), plantFossil),
+                DynamicLoot.dynamicEntry(ModItemTags.LEG_BONES.location()).setWeight(weights[0]),
+                DynamicLoot.dynamicEntry(ModItemTags.ARM_BONES.location()).setWeight(weights[1]),
+                DynamicLoot.dynamicEntry(ModItemTags.FOOT_BONES.location()).setWeight(weights[2]),
+                DynamicLoot.dynamicEntry(ModItemTags.SKULL_BONES.location()).setWeight(weights[3]),
+                DynamicLoot.dynamicEntry(ModItemTags.RIBCAGE_BONES.location()).setWeight(weights[4]),
+                DynamicLoot.dynamicEntry(ModItemTags.VERTEBRAE_BONES.location()).setWeight(weights[5]),
+                DynamicLoot.dynamicEntry(ModItemTags.UNIQUE_BONES.location()).setWeight(weights[6]),
+                DynamicLoot.dynamicEntry(ModItemTags.TAIL_BONES.location()).setWeight(weights[7]));
     }
 
     private LootPoolSingletonContainer.Builder<?> lootItem(ItemLike item, int weight) {
