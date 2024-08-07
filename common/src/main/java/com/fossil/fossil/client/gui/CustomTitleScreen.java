9b6a7960fb3993e2e2b6a82d7b9c47f838ab4515
DarkPred <26512194+DarkPred@users.noreply.github.com>
2024-08-07 17:25:59 +0200

    Add parallax title screen

---
+++ common/src/main/java/com/fossil/fossil/client/gui/CustomTitleScreen.java
@@ -0,0 +1,8 @@
+package com.fossil.fossil.client.gui;
+
+import com.fossil.fossil.Fossil;
+import net.minecraft.resources.ResourceLocation;
+public class CustomTitleScreen {
+    public static final ResourceLocation LAYER_TEXTURE_BACK = new ResourceLocation(Fossil.MOD_ID, "textures/gui/parallax/layer_0.png");
+    public static final ResourceLocation LAYER_TEXTURE_FRONT = new ResourceLocation(Fossil.MOD_ID, "textures/gui/parallax/layer_1.png");
+}
