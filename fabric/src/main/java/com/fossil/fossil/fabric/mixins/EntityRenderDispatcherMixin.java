0e518efdecc2a73619ce0ed401a2780b54765b4b
DarkPred <26512194+DarkPred@users.noreply.github.com>
2024-08-27 14:21:44 +0200

    Add active attack box to debug renderer

--- fabric/src/main/java/com/fossil/fossil/fabric/mixins/EntityRenderDispatcherMixin.java
+++ fabric/src/main/java/com/fossil/fossil/fabric/mixins/EntityRenderDispatcherMixin.java
@@ -1,5 +1,6 @@
 package com.fossil.fossil.fabric.mixins;
 
+import com.fossil.fossil.entity.data.EntityHitboxManager;
 import com.fossil.fossil.entity.prehistoric.base.Prehistoric;
 import com.fossil.fossil.entity.prehistoric.parts.MultiPart;
 import com.fossil.fossil.util.Version;
@@ -7,16 +8,20 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.mojang.math.Matrix3f;
 import com.mojang.math.Matrix4f;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.LevelRenderer;
 import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityDimensions;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Map;
 
 /**
  * Used for debugging multiple hitboxes
@@ -31,17 +36,34 @@ public abstract class EntityRenderDispatcherMixin {
             //Unchanged original code
             AABB aABB = entity.getBoundingBox().move(-entity.getX(), -entity.getY(), -entity.getZ());
             LevelRenderer.renderLineBox(poseStack, buffer, aABB, 1, 1, 1, 1);
-            if (Version.debugEnabled()) {
-                aABB = entity.getBoundingBoxForCulling().move(-entity.getX(), -entity.getY(), -entity.getZ());
-                LevelRenderer.renderLineBox(poseStack, buffer, aABB, 1, 0, 1, 1);
-                aABB = ((Prehistoric) entity).getAttackBounds().move(-entity.getX(), -entity.getY(), -entity.getZ());
-                LevelRenderer.renderLineBox(poseStack, buffer, aABB, 0, 0, 1, 1);
-            }
 
             //Changed code
             double d = -Mth.lerp(partialTicks, entity.xOld, entity.getX());
             double e = -Mth.lerp(partialTicks, entity.yOld, entity.getY());
             double f = -Mth.lerp(partialTicks, entity.zOld, entity.getZ());
+            if (Version.debugEnabled()) {
+                aABB = entity.getBoundingBoxForCulling().move(-entity.getX(), -entity.getY(), -entity.getZ());
+                LevelRenderer.renderLineBox(poseStack, buffer, aABB, 1, 0, 1, 1);
+                aABB = ((Prehistoric) entity).getAttackBounds().move(-entity.getX(), -entity.getY(), -entity.getZ());
+                LevelRenderer.renderLineBox(poseStack, buffer, aABB, 0, 0, 1, 1);
+                for (Map.Entry<EntityHitboxManager.Hitbox, Vec3> entry : prehistoric.activeAttackBoxes.entrySet()) {
+                    Vec3 pos = entry.getValue();
+                    EntityHitboxManager.Hitbox hitbox = entry.getKey();
+                    EntityDimensions size = EntityDimensions.scalable(hitbox.width(), hitbox.height()).scale(prehistoric.getScale());
+                    AABB aabb = size.makeBoundingBox(pos);
+                    poseStack.pushPose();
+                    double g = d + pos.x;
+                    double h = e + pos.y;
+                    double i = f + pos.z;
+                    poseStack.translate(g, h, i);
+                    if (Minecraft.getInstance().player.getBoundingBox().intersects(aabb)) {
+                        LevelRenderer.renderLineBox(poseStack, buffer, aabb.move(-pos.x, -pos.y, -pos.z), 1, 0, 0, 1);
+                    } else {
+                        LevelRenderer.renderLineBox(poseStack, buffer, aabb.move(-pos.x, -pos.y, -pos.z), 0, 0, 1, 1);
+                    }
+                    poseStack.popPose();
+                }
+            }
             int kj = 0;
             for (MultiPart multiPart : prehistoric.getCustomParts()) {
                 Entity part = multiPart.getEntity();
