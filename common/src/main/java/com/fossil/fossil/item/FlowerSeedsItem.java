f4b3e6e15f7d4adbeac77d4a43ec4f05c92400ed
DarkPred <26512194+DarkPred@users.noreply.github.com>
2024-08-31 16:51:38 +0200

    Fix flower seed placement

--- common/src/main/java/com/fossil/fossil/item/FlowerSeedsItem.java
+++ common/src/main/java/com/fossil/fossil/item/FlowerSeedsItem.java
@@ -6,6 +6,7 @@ import dev.architectury.registry.registries.RegistrySupplier;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BushBlock;
@@ -22,26 +23,30 @@ public class FlowerSeedsItem extends Item {
 
     @Override
     public @NotNull InteractionResult useOn(UseOnContext context) {
+        BlockPlaceContext blockPlaceContext  = new BlockPlaceContext(context);
+        if (!blockPlaceContext.canPlace()) {
+            return InteractionResult.FAIL;
+        }
         int clearance = 1;
         if (flower.get() instanceof TallFlowerBlock) {
             clearance = 2;
         } else if (flower.get() instanceof FourTallFlowerBlock) {
             clearance = 4;
         }
-        BlockPos pos = context.getClickedPos();
+        BlockPos pos = blockPlaceContext.getClickedPos();
         Level level = context.getLevel();
         for (int i = 0; i < clearance; i++) {
-            BlockPos up = pos.above(1 + i);
+            BlockPos up = pos.above(i);
             if (!level.isEmptyBlock(up)) {
                 return InteractionResult.PASS;
             }
         }
         if (flower.get().defaultBlockState().canSurvive(level, pos)) {
-            level.setBlock(pos.above(), flower.get().defaultBlockState(), 3);
+            level.setBlock(pos, flower.get().defaultBlockState(), 3);
             if (flower.get() instanceof DoublePlantBlock tall) {
-                tall.setPlacedBy(level, pos.above(), null, null, null);
+                tall.setPlacedBy(level, pos, null, null, null);
             } else if (flower.get() instanceof FourTallFlowerBlock tall) {
-                tall.setPlacedBy(level, pos.above(), null, null, null);
+                tall.setPlacedBy(level, pos, null, null, null);
             }
             context.getItemInHand().shrink(1);
             return InteractionResult.SUCCESS;
