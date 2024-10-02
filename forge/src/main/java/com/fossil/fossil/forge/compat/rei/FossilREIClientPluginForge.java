3915db57657baecf5dbe301bb0f3446bb1d260df
DarkPred <26512194+DarkPred@users.noreply.github.com>
2024-10-02 02:43:23 +0200

    Add forge rei plugin classes

---
+++ forge/src/main/java/com/fossil/fossil/forge/compat/rei/FossilREIClientPluginForge.java
@@ -0,0 +1,8 @@
+package com.fossil.fossil.forge.compat.rei;
+
+import com.fossil.fossil.compat.rei.FossilREIClientPlugin;
+import me.shedaniel.rei.forge.REIPluginClient;
+
+@REIPluginClient
+public class FossilREIClientPluginForge extends FossilREIClientPlugin {
+}
---
+++ forge/src/main/java/com/fossil/fossil/forge/compat/rei/FossilREICommonPluginForge.java
@@ -0,0 +1,8 @@
+package com.fossil.fossil.forge.compat.rei;
+
+import com.fossil.fossil.compat.rei.FossilREICommonPlugin;
+import me.shedaniel.rei.forge.REIPluginCommon;
+
+@REIPluginCommon
+public class FossilREICommonPluginForge extends FossilREICommonPlugin {
+}
