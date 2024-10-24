817a344f5f3fab1e7addefef11247a04351ef08d
DarkPred <26512194+DarkPred@users.noreply.github.com>
2024-10-25 01:47:00 +0200

    Fix iris/indium compat

--- fabric/src/main/java/com/github/teamfossilsarcheology/fossil/fabric/client/model/FabricPlantUnbakedModel.java
+++ fabric/src/main/java/com/github/teamfossilsarcheology/fossil/fabric/client/model/FabricPlantUnbakedModel.java
@@ -7,9 +7,9 @@ import com.github.teamfossilsarcheology.fossil.client.model.block.PlantModelBake
 import com.mojang.datafixers.util.Pair;
 import net.fabricmc.fabric.api.renderer.v1.Renderer;
 import net.fabricmc.fabric.api.renderer.v1.RendererAccess;
+import net.fabricmc.fabric.api.renderer.v1.material.RenderMaterial;
 import net.fabricmc.fabric.api.renderer.v1.mesh.MeshBuilder;
 import net.fabricmc.fabric.api.renderer.v1.mesh.QuadEmitter;
-import net.fabricmc.fabric.impl.client.indigo.renderer.IndigoRenderer;
 import net.minecraft.client.renderer.block.model.BakedQuad;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.client.resources.model.*;
@@ -24,6 +24,7 @@ import java.util.Set;
 import java.util.function.Function;
 
 public class FabricPlantUnbakedModel implements UnbakedModel {
+    public static final RenderMaterial MATERIAL_STANDARD = RendererAccess.INSTANCE.getRenderer().materialFinder().find();
 
     private final PlantBlockModel model;
 
@@ -55,7 +56,7 @@ public class FabricPlantUnbakedModel implements UnbakedModel {
                 TextureAtlasSprite texture = spriteGetter.apply(model.materials().get(face.texture().substring(1)));
                 BakedQuad bakedQuad = PlantModelBakery.bakeFace(element, face, texture, direction, modelState);
                 //I'm sure that this isn't the best, but it's really easy
-                emitter.fromVanilla(bakedQuad, IndigoRenderer.MATERIAL_STANDARD, direction);
+                emitter.fromVanilla(bakedQuad, MATERIAL_STANDARD, direction);
                 emitter.cullFace(null);
                 emitter.nominalFace(direction);
                 emitter.emit();
