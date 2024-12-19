4bcb9d57df4ed27ba71cc3fc821d47ce72ec432c
DarkPred <26512194+DarkPred@users.noreply.github.com>
2024-12-19 23:13:47 +0100

    Add more dev capes

--- common/src/main/java/com/github/teamfossilsarcheology/fossil/client/FAPlayerCapes.java
+++ common/src/main/java/com/github/teamfossilsarcheology/fossil/client/FAPlayerCapes.java
@@ -9,10 +9,12 @@ import java.util.UUID;
 public class FAPlayerCapes {
     public static final Set<UUID> DEV_UUIDS = Set.of(
             /* Dark_Pred */UUID.fromString("f831e2f6-9670-46af-b1c0-e1e1a9fa6086"),
+            /* dino_dan_ */UUID.fromString("fe4efe89-82eb-4c9e-bd35-f661147209f8"),
             /* Dipple_Effect */UUID.fromString("81a2e754-9f39-487d-8e1c-bf5311db79ec"),
             /* GinjaninjaS7 */UUID.fromString("18eb6ad8-1656-4e41-89f6-88b708a0474c"),
             /* Robberto08 */UUID.fromString("05b14ce7-0ff1-4b8e-9ef8-d98502e9bf07"),
             /* SeaManVI */UUID.fromString("fe87a60b-fafe-4bec-ab92-6f489d99e0c4"),
+            /* TheTotemaster */UUID.fromString("d3b6e383-53ba-40cd-bf7a-c073e0fe39f7"),
             /* TotallyAHobo */UUID.fromString("e933f353-87d3-4bdb-8afe-bddb4c31b68f")
     );
     public static final Set<UUID> CONTRIBUTOR_UUIDS = Set.of(
@@ -23,13 +25,14 @@ public class FAPlayerCapes {
             /* Helpimdeadinside */UUID.fromString("5182bc80-8680-467a-8c2f-6127231a1977"),
             /* Klubarian */UUID.fromString("57f1201f-5293-495d-a3fb-aa92680ee92a"),
             /* Krimpomp */UUID.fromString("7a4804fe-a6df-4243-9076-ff719f5ec627"),
+            /* Larissa___ */UUID.fromString("6e5f5923-186b-484f-a31e-a4b3e74fd28c"),
             /* nanotyrano */UUID.fromString("d4c29faf-82bc-4d34-bda0-cb850595595a"),
             /* NotThatHyper */UUID.fromString("13d60612-a851-4945-bf88-4183dd281d7a"),
             /* Poecilokillia */ UUID.fromString("33ec43ff-ea27-4a6b-a4c0-bedde458488c"),
             /* RecklessBuddha */UUID.fromString("f17aeda8-bb84-44fa-b2fe-f0c9183c603b"),
             /* SJ_Code */UUID.fromString("1f1b6f21-5314-41b5-8482-3f7b0f07303b"),
-            /* TheTotemaster */UUID.fromString("d3b6e383-53ba-40cd-bf7a-c073e0fe39f7"),
             /* tmvkrpxl0 */UUID.fromString("b6c7411f-15e2-48f6-b53f-876ec0577e82"),
+            /* Toastlord12 */UUID.fromString("db535335-c024-4f19-bfce-0fd531c79fa4"),
             /* _W0r1d_ */UUID.fromString("b4df5358-4596-4b03-a2b8-048ac15e401a")
     );
     public static final ResourceLocation DEVELOPER_CAPE_TEXTURE = FossilMod.location("textures/skins/developer_cape.png");
