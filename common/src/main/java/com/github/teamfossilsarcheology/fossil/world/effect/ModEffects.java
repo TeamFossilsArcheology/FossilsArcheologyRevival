6b48fea0ccf8ee43f0a3dae99d358d77265d8716
DarkPred <26512194+DarkPred@users.noreply.github.com>
2025-01-12 07:50:55 +0100

    Update comfy bed effect colour

--- common/src/main/java/com/github/teamfossilsarcheology/fossil/world/effect/ModEffects.java
+++ common/src/main/java/com/github/teamfossilsarcheology/fossil/world/effect/ModEffects.java
@@ -11,7 +11,7 @@ public class ModEffects {
 
     public static final DeferredRegister<MobEffect> MOB_EFFECTS = DeferredRegister.create(FossilMod.MOD_ID, Registry.MOB_EFFECT_REGISTRY);
 
-    public static final RegistrySupplier<MobEffect> COMFY_BED = MOB_EFFECTS.register("comfy_bed", () -> new ComfyBedEffect(MobEffectCategory.BENEFICIAL, 8954814));
+    public static final RegistrySupplier<MobEffect> COMFY_BED = MOB_EFFECTS.register("comfy_bed", () -> new ComfyBedEffect(MobEffectCategory.BENEFICIAL, 0x70573d));
 
     public static void register() {
         MOB_EFFECTS.register();
