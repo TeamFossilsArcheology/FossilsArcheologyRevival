d3bc924f62c253da7d20de10739936f30cd7655b
DarkPred <26512194+DarkPred@users.noreply.github.com>
2025-01-28 21:33:15 +0100

    Fix main menu config option

--- forge/src/main/java/com/github/teamfossilsarcheology/fossil/forge/mixin/TitleScreenMixin.java
+++ forge/src/main/java/com/github/teamfossilsarcheology/fossil/forge/mixin/TitleScreenMixin.java
@@ -1,5 +1,6 @@
 package com.github.teamfossilsarcheology.fossil.forge.mixin;
 
+import com.github.teamfossilsarcheology.fossil.config.FossilConfig;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.gui.screens.Screen;
@@ -34,25 +35,33 @@ public abstract class TitleScreenMixin extends Screen {
 
     @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/PanoramaRenderer;render(FF)V"))
     protected void r(PoseStack poseStack, int mouseX, int mouseY, float partialTick, CallbackInfo ci) {
-        fossil$layerTick++;
-        RenderSystem.setShader(GameRenderer::getPositionTexShader);
-        RenderSystem.enableTexture();
-        RenderSystem.setShaderColor(1, 1, 1, 1);
-        RenderSystem.enableBlend();
-        RenderSystem.setShaderTexture(0, LAYER_TEXTURE_BACK);
-        float u = fossil$initialOffsetBack + ((fossil$layerTick + partialTick) / 2f) + 1;
-        blit(poseStack, 0, 0, u / (960f / width), 0, width, height, (int) (1024 * (height / 128f)), height);
+        if (FossilConfig.isEnabled(FossilConfig.CUSTOM_MAIN_MENU)) {
+            fossil$layerTick++;
+            RenderSystem.setShader(GameRenderer::getPositionTexShader);
+            RenderSystem.enableTexture();
+            RenderSystem.setShaderColor(1, 1, 1, 1);
+            RenderSystem.enableBlend();
+            RenderSystem.setShaderTexture(0, LAYER_TEXTURE_BACK);
+            float u = fossil$initialOffsetBack + ((fossil$layerTick + partialTick) / 2f) + 1;
+            blit(poseStack, 0, 0, u / (960f / width), 0, width, height, (int) (1024 * (height / 128f)), height);
 
-        RenderSystem.setShaderTexture(0, LAYER_TEXTURE_FRONT);
-        u = fossil$initialOffsetFront + fossil$layerTick + partialTick + 2 + 512;
-        blit(poseStack, 0, 0, u / (960f / width), 0, width, height, (int) (2048 * (height / 128f)), height);
+            RenderSystem.setShaderTexture(0, LAYER_TEXTURE_FRONT);
+            u = fossil$initialOffsetFront + fossil$layerTick + partialTick + 2 + 512;
+            blit(poseStack, 0, 0, u / (960f / width), 0, width, height, (int) (2048 * (height / 128f)), height);
+        }
     }
 
     @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/PanoramaRenderer;render(FF)V"))
     protected void removePanorama(PanoramaRenderer instance, float deltaT, float alpha) {
+        if (!FossilConfig.isEnabled(FossilConfig.CUSTOM_MAIN_MENU)) {
+            instance.render(deltaT, alpha);
+        }
     }
 
     @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screens/TitleScreen;blit(Lcom/mojang/blaze3d/vertex/PoseStack;IIIIFFIIII)V"))
     protected void removeOverlay(PoseStack poseStack, int a, int b, int c, int d, float e, float f, int g, int h, int i, int j) {
+        if (!FossilConfig.isEnabled(FossilConfig.CUSTOM_MAIN_MENU)) {
+            blit(poseStack, 0, 0, this.width, this.height, 0.0F, 0.0F, 16, 128, 16, 128);
+        }
     }
 }
