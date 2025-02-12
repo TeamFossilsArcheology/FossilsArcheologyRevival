7a5d3508a0f5e8ce82da21b0a1d804a8e9129f0d
DarkPred <26512194+DarkPred@users.noreply.github.com>
2025-02-12 03:11:27 +0100

    Fix trapdoor render type

--- common/src/main/java/com/github/teamfossilsarcheology/fossil/client/ClientInit.java
+++ common/src/main/java/com/github/teamfossilsarcheology/fossil/client/ClientInit.java
@@ -355,35 +355,35 @@ public class ClientInit {
         RenderTypeRegistry.register(RenderType.cutout(), ModBlocks.CALAMITES_DOOR.get());
         RenderTypeRegistry.register(RenderType.cutout(), ModBlocks.CALAMITES_LEAVES.get());
         RenderTypeRegistry.register(RenderType.cutout(), ModBlocks.CALAMITES_SAPLING.get());
-        RenderTypeRegistry.register(RenderType.translucent(), ModBlocks.CALAMITES_TRAPDOOR.get());
+        RenderTypeRegistry.register(RenderType.cutout(), ModBlocks.CALAMITES_TRAPDOOR.get());
 
         RenderTypeRegistry.register(RenderType.cutout(), ModBlocks.CORDAITES_DOOR.get());
         RenderTypeRegistry.register(RenderType.cutout(), ModBlocks.CORDAITES_LEAVES.get());
         RenderTypeRegistry.register(RenderType.cutout(), ModBlocks.CORDAITES_SAPLING.get());
-        RenderTypeRegistry.register(RenderType.translucent(), ModBlocks.CORDAITES_TRAPDOOR.get());
+        RenderTypeRegistry.register(RenderType.cutout(), ModBlocks.CORDAITES_TRAPDOOR.get());
 
         RenderTypeRegistry.register(RenderType.cutout(), ModBlocks.MUTANT_TREE_DOOR.get());
         RenderTypeRegistry.register(RenderType.cutout(), ModBlocks.MUTANT_TREE_LEAVES.get());
         RenderTypeRegistry.register(RenderType.cutout(), ModBlocks.MUTANT_TREE_SAPLING.get());
-        RenderTypeRegistry.register(RenderType.translucent(), ModBlocks.MUTANT_TREE_TRAPDOOR.get());
+        RenderTypeRegistry.register(RenderType.cutout(), ModBlocks.MUTANT_TREE_TRAPDOOR.get());
         RenderTypeRegistry.register(RenderType.cutout(), ModBlocks.MUTANT_TREE_TUMOR.get());
         RenderTypeRegistry.register(RenderType.cutout(), ModBlocks.MUTANT_TREE_VINE.get());
 
         RenderTypeRegistry.register(RenderType.cutout(), ModBlocks.PALM_DOOR.get());
         RenderTypeRegistry.register(RenderType.cutout(), ModBlocks.PALM_LEAVES.get());
         RenderTypeRegistry.register(RenderType.cutout(), ModBlocks.PALM_SAPLING.get());
-        RenderTypeRegistry.register(RenderType.translucent(), ModBlocks.PALM_TRAPDOOR.get());
+        RenderTypeRegistry.register(RenderType.cutout(), ModBlocks.PALM_TRAPDOOR.get());
 
         RenderTypeRegistry.register(RenderType.cutout(), ModBlocks.SIGILLARIA_DOOR.get());
         RenderTypeRegistry.register(RenderType.cutout(), ModBlocks.SIGILLARIA_LEAVES.get());
         RenderTypeRegistry.register(RenderType.cutout(), ModBlocks.SIGILLARIA_SAPLING.get());
-        RenderTypeRegistry.register(RenderType.translucent(), ModBlocks.SIGILLARIA_TRAPDOOR.get());
+        RenderTypeRegistry.register(RenderType.cutout(), ModBlocks.SIGILLARIA_TRAPDOOR.get());
 
         RenderTypeRegistry.register(RenderType.cutout(), ModBlocks.TEMPSKYA_DOOR.get());
         RenderTypeRegistry.register(RenderType.cutout(), ModBlocks.TEMPSKYA_SAPLING.get());
         RenderTypeRegistry.register(RenderType.cutout(), ModBlocks.TEMPSKYA_TOP.get());
         RenderTypeRegistry.register(RenderType.cutout(), ModBlocks.TEMPSKYA_LEAF.get());
-        RenderTypeRegistry.register(RenderType.translucent(), ModBlocks.TEMPSKYA_TRAPDOOR.get());
+        RenderTypeRegistry.register(RenderType.cutout(), ModBlocks.TEMPSKYA_TRAPDOOR.get());
 
         RenderTypeRegistry.register(RenderType.cutout(), ModBlocks.SLIME_TRAIL.get());
         RenderTypeRegistry.register(RenderType.cutout(), ModBlocks.OBSIDIAN_SPIKES.get());
