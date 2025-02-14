77c7e638b5401305fa72fae87d35d3ad11f021ba
DarkPred <26512194+DarkPred@users.noreply.github.com>
2025-02-14 22:29:49 +0100

    Try fix to prevent aquatic mobs from swimming in circles and fix mosa sleep anim

--- common/src/main/java/com/github/teamfossilsarcheology/fossil/entity/ai/control/CustomSwimMoveControl.java
+++ common/src/main/java/com/github/teamfossilsarcheology/fossil/entity/ai/control/CustomSwimMoveControl.java
@@ -37,6 +37,9 @@ public class CustomSwimMoveControl<T extends Prehistoric & SwimmingAnimal> exten
                 }
                 if (mob.isInWater()) {
                     float i = (float) mob.swimSpeed();
+                    if (horizontalDist < 4) {
+                        i *= 0.5f;
+                    }
                     mob.setSpeed(i);
                     double horDist = Math.sqrt(x * x + z * z);
                     float k;
