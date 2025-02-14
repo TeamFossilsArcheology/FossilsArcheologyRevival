0f59e567284b151cd7d41be4c4f340f4b4bccd22
DarkPred <26512194+DarkPred@users.noreply.github.com>
2025-02-14 20:56:00 +0100

    Fix sentry piglin spawning without weapon and anky attack hitbox not working

--- common/src/main/java/com/github/teamfossilsarcheology/fossil/entity/monster/SentryPiglin.java
+++ common/src/main/java/com/github/teamfossilsarcheology/fossil/entity/monster/SentryPiglin.java
@@ -1,16 +1,21 @@
 package com.github.teamfossilsarcheology.fossil.entity.monster;
 
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.MobSpawnType;
+import net.minecraft.world.entity.SpawnGroupData;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.world.entity.monster.Zombie;
+import net.minecraft.world.entity.ai.navigation.GroundPathNavigation;
 import net.minecraft.world.entity.monster.piglin.PiglinBrute;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.ServerLevelAccessor;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class SentryPiglin extends PiglinBrute {
 
@@ -20,11 +25,21 @@ public class SentryPiglin extends PiglinBrute {
     }
 
     public static AttributeSupplier.@NotNull Builder createAttributes() {
-        return Zombie.createMonsterAttributes().add(Attributes.MAX_HEALTH, 20).add(Attributes.MOVEMENT_SPEED, 0.3f).add(Attributes.ATTACK_DAMAGE, 5).add(Attributes.ARMOR, 8);
+        return createMonsterAttributes().add(Attributes.MAX_HEALTH, 20).add(Attributes.MOVEMENT_SPEED, 0.3f).add(Attributes.ATTACK_DAMAGE, 5).add(Attributes.ARMOR, 8);
     }
 
     @Override
     protected void populateDefaultEquipmentSlots(DifficultyInstance difficulty) {
         setItemSlot(EquipmentSlot.MAINHAND, new ItemStack(Items.IRON_SWORD));
+    }
+
+    @Nullable
+    @Override
+    public SpawnGroupData finalizeSpawn(ServerLevelAccessor level, DifficultyInstance difficulty, MobSpawnType reason, @Nullable SpawnGroupData spawnData, @Nullable CompoundTag dataTag) {
+        SpawnGroupData spawnGroupData = super.finalizeSpawn(level, difficulty, reason, spawnData, dataTag);
+        ((GroundPathNavigation) getNavigation()).setCanOpenDoors(true);
+        populateDefaultEquipmentSlots(difficulty);
+        populateDefaultEquipmentEnchantments(difficulty);
+        return spawnGroupData;
     }
 }
