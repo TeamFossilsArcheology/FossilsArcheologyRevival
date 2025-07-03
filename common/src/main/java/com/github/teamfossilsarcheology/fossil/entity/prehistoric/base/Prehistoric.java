d3abb1ca47368a1b3d4bf12dbbf028b38572671d
DarkPred <26512194+DarkPred@users.noreply.github.com>
2025-07-03 18:06:40 +0200

    Fix config conditions not applying on first mob spawn

--- common/src/main/java/com/github/teamfossilsarcheology/fossil/entity/prehistoric/base/Prehistoric.java
+++ common/src/main/java/com/github/teamfossilsarcheology/fossil/entity/prehistoric/base/Prehistoric.java
@@ -457,6 +457,14 @@ public abstract class Prehistoric extends TamableAnimal implements GeckoLibMulti
         heal(getMaxHealth());
         setCurrentOrder(OrderType.WANDER);
         setNoAi(false);
+        for (VariantCondition.WithVariant<ConfigCondition> pair : variantsByCondition(ConfigCondition.class)) {
+            if (pair.condition().test()) {
+                setVariant(VariantRegistry.CONFIG, pair);
+                break;
+            } else if (allVariants.containsKey(VariantRegistry.CONFIG) && Objects.equals(allVariants.get(VariantRegistry.CONFIG).condition(), pair.condition())) {
+                clearVariant(VariantRegistry.CONFIG);
+            }
+        }
         for (VariantCondition.WithVariant<DateCondition> pair : variantsByCondition(DateCondition.class)) {
             if (pair.condition().test(random, ZonedDateTime.now())) {
                 setVariant(VariantRegistry.DATE, pair);
