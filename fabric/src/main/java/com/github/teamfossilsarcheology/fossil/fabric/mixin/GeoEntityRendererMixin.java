44e53faceb6a64f0fa705756ab24be1a1de9e7d2
DarkPred <26512194+DarkPred@users.noreply.github.com>
2025-07-06 19:46:57 +0200

    Fix GeoEntityRendererMixin

--- fabric/src/main/java/com/github/teamfossilsarcheology/fossil/fabric/mixin/GeoEntityRendererMixin.java
+++ fabric/src/main/java/com/github/teamfossilsarcheology/fossil/fabric/mixin/GeoEntityRendererMixin.java
@@ -2,20 +2,20 @@ package com.github.teamfossilsarcheology.fossil.fabric.mixin;
 
 import com.github.teamfossilsarcheology.fossil.entity.prehistoric.base.PrehistoricLeaping;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import net.minecraft.world.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import software.bernie.geckolib.core.animatable.GeoAnimatable;
 import software.bernie.geckolib.renderer.GeoEntityRenderer;
 
 @Mixin(GeoEntityRenderer.class)
 public abstract class GeoEntityRendererMixin {
 
     @Shadow
-    protected GeoAnimatable animatable;
+    protected Entity animatable;
 
     @ModifyExpressionValue(method = "actuallyRender(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/world/entity/Entity;Lsoftware/bernie/geckolib/cache/object/BakedGeoModel;Lnet/minecraft/client/renderer/RenderType;Lnet/minecraft/client/renderer/MultiBufferSource;Lcom/mojang/blaze3d/vertex/VertexConsumer;ZFIIFFFF)V",
-            at = @At(value = "INVOKE", remap = false, target = "Lnet/minecraft/world/entity/Entity;isPassenger()Z"))
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;isPassenger()Z"))
     public boolean disableSitting(boolean original) {
         if (animatable instanceof PrehistoricLeaping leaping && leaping.getLeapSystem().isAttackRiding()) {
             return false;
--- forge/src/main/java/com/github/teamfossilsarcheology/fossil/forge/mixin/GeoEntityRendererMixin.java
+++ forge/src/main/java/com/github/teamfossilsarcheology/fossil/forge/mixin/GeoEntityRendererMixin.java
@@ -2,17 +2,17 @@ package com.github.teamfossilsarcheology.fossil.forge.mixin;
 
 import com.github.teamfossilsarcheology.fossil.entity.prehistoric.base.PrehistoricLeaping;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import net.minecraft.world.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import software.bernie.geckolib.core.animatable.GeoAnimatable;
 import software.bernie.geckolib.renderer.GeoEntityRenderer;
 
 @Mixin(GeoEntityRenderer.class)
 public class GeoEntityRendererMixin {
 
     @Shadow
-    protected GeoAnimatable animatable;
+    protected Entity animatable;
 
     @ModifyExpressionValue(method = "actuallyRender(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/world/entity/Entity;Lsoftware/bernie/geckolib/cache/object/BakedGeoModel;Lnet/minecraft/client/renderer/RenderType;Lnet/minecraft/client/renderer/MultiBufferSource;Lcom/mojang/blaze3d/vertex/VertexConsumer;ZFIIFFFF)V",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;isPassenger()Z"))
