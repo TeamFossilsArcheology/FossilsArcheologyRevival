4c16a73b67973f974e1e607bee906d0e0f321e9c
DarkPred <26512194+DarkPred@users.noreply.github.com>
2025-08-22 20:09:33 +0200

    Fix ancient helmet rendering

--- common/src/main/java/com/github/teamfossilsarcheology/fossil/client/renderer/armor/AncientHelmetRenderer.java
+++ common/src/main/java/com/github/teamfossilsarcheology/fossil/client/renderer/armor/AncientHelmetRenderer.java
@@ -1,11 +1,12 @@
 package com.github.teamfossilsarcheology.fossil.client.renderer.armor;
 
+import com.github.teamfossilsarcheology.fossil.FossilMod;
 import com.github.teamfossilsarcheology.fossil.item.AncientHelmetItem;
 import software.bernie.geckolib.model.DefaultedItemGeoModel;
 import software.bernie.geckolib.renderer.GeoArmorRenderer;
 
 public class AncientHelmetRenderer extends GeoArmorRenderer<AncientHelmetItem> {
     public AncientHelmetRenderer() {
-        super(new DefaultedItemGeoModel<>(AncientHelmetItem.MODEL));
+        super(new DefaultedItemGeoModel<>(FossilMod.location("armor/ancient_helmet")));
     }
 }
--- common/src/main/java/com/github/teamfossilsarcheology/fossil/item/AncientHelmetItem.java
+++ common/src/main/java/com/github/teamfossilsarcheology/fossil/item/AncientHelmetItem.java
@@ -1,8 +1,6 @@
 package com.github.teamfossilsarcheology.fossil.item;
 
-import com.github.teamfossilsarcheology.fossil.FossilMod;
 import dev.architectury.injectables.annotations.ExpectPlatform;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ArmorMaterial;
@@ -13,8 +11,6 @@ import software.bernie.geckolib.core.animation.AnimatableManager;
 import software.bernie.geckolib.util.GeckoLibUtil;
 
 public abstract class AncientHelmetItem extends ArmorItem implements GeoItem {
-    public static final ResourceLocation MODEL = FossilMod.location("geo/armor/ancient_helmet.geo.json");
-    public static final ResourceLocation TEXTURE = FossilMod.location("textures/models/armor/ancient_helmet_texture.png");
     private final AnimatableInstanceCache cache = GeckoLibUtil.createInstanceCache(this);
 
     protected AncientHelmetItem(ArmorMaterial material, EquipmentSlot slot, Properties properties) {
