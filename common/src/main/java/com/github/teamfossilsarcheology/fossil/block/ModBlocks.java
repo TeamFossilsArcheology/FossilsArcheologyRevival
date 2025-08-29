b9df3ff0586e6abe482843876dca264777c95531
DarkPred <26512194+DarkPred@users.noreply.github.com>
2025-08-29 19:08:41 +0200

    Fix having a strength of 0

--- common/src/main/java/com/github/teamfossilsarcheology/fossil/block/ModBlocks.java
+++ common/src/main/java/com/github/teamfossilsarcheology/fossil/block/ModBlocks.java
@@ -381,7 +381,7 @@ public class ModBlocks {
     }
 
     private static RegistrySupplier<RotatedPillarBlock> log(ModWoodTypes.WoodInfo woodInfo, boolean stripped) {
-        return rotatedPillar((stripped ? "stripped_" : "") + woodInfo.name() + "_log", BlockBehaviour.Properties.of(Material.WOOD, woodInfo.materialColor()));
+        return rotatedPillar((stripped ? "stripped_" : "") + woodInfo.name() + "_log", woodProp(woodInfo).strength(2));
     }
 
     private static RegistrySupplier<RotatedPillarBlock> wood(ModWoodTypes.WoodInfo woodInfo, boolean stripped) {
