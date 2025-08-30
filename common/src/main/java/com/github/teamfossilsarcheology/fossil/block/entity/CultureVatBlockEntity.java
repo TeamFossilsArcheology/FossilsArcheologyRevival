37402f31eeed0f635b2921dcf5e0f6cabeb08149
DarkPred <26512194+DarkPred@users.noreply.github.com>
2025-08-30 20:56:53 +0200

    Fix culture vat and worktable sometimes crashing the game

--- common/src/main/java/com/github/teamfossilsarcheology/fossil/block/entity/CultureVatBlockEntity.java
+++ common/src/main/java/com/github/teamfossilsarcheology/fossil/block/entity/CultureVatBlockEntity.java
@@ -125,7 +125,7 @@ public class CultureVatBlockEntity extends EnergyContainerBlockEntity {
             if (litTime > 0) {
                 dirty = true;
                 fuelStack.shrink(1);
-                if (fuelStack.isEmpty()) {
+                if (fuelStack.isEmpty() && fuelStack.getItem().hasCraftingRemainingItem()) {
                     items.set(CultureVatMenu.FUEL_SLOT_ID, new ItemStack(fuelStack.getItem().getCraftingRemainingItem()));
                 }
             }
--- common/src/main/java/com/github/teamfossilsarcheology/fossil/block/entity/WorktableBlockEntity.java
+++ common/src/main/java/com/github/teamfossilsarcheology/fossil/block/entity/WorktableBlockEntity.java
@@ -97,7 +97,7 @@ public class WorktableBlockEntity extends MachineContainerBlockEntity {
             if (litTime > 0) {
                 dirty = true;
                 fuelStack.shrink(1);
-                if (fuelStack.isEmpty()) {
+                if (fuelStack.isEmpty() && fuelStack.getItem().hasCraftingRemainingItem()) {
                     items.set(WorktableMenu.FUEL_SLOT_ID, new ItemStack(fuelStack.getItem().getCraftingRemainingItem()));
                 }
             }
