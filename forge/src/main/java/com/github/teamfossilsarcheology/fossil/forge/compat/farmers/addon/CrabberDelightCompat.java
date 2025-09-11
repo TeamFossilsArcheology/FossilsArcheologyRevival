0c1b4cc03e736191c62710345875bc0d16d5169d
DarkPred <26512194+DarkPred@users.noreply.github.com>
2025-09-11 18:33:52 +0200

    Update minimum version for crabbers delight (fixes #80)

--- forge/src/main/java/com/github/teamfossilsarcheology/fossil/forge/compat/farmers/addon/CrabberDelightCompat.java
+++ forge/src/main/java/com/github/teamfossilsarcheology/fossil/forge/compat/farmers/addon/CrabberDelightCompat.java
@@ -1,32 +1,32 @@
 package com.github.teamfossilsarcheology.fossil.forge.compat.farmers.addon;
 
-import alabaster.crabbersdelight.common.registry.ModItems;
+import alabaster.crabbersdelight.common.registry.CDModItems;
 import com.github.teamfossilsarcheology.fossil.food.FoodMappingsManager;
 
 public class CrabberDelightCompat {
     public static void registerFoodMappings(FoodMappingsManager manager) {
-        manager.addFish(ModItems.BISQUE.get()); //Dish made of fish+plants, so I considered it a fish
-        manager.addFish(ModItems.CLAM_BAKE.get()); //Dish made of fish+plants, so I considered it a fish
-        manager.addMeat(ModItems.CLAM_CHOWDER.get()); //Dish made of clam meat+plants, so I considered it a meat
-        manager.addMeat(ModItems.COOKED_CLAM_MEAT.get()); //Clam meat so meat
-        manager.addFish(ModItems.COOKED_CLAWSTER.get());
-        manager.addFish(ModItems.COOKED_CRAB.get());
-        manager.addFish(ModItems.COOKED_SHRIMP.get());
-        manager.addFish(ModItems.COOKED_TROPICAL_FISH.get());
-        manager.addFish(ModItems.COOKED_TROPICAL_FISH_SLICE.get());
-        manager.addFish(ModItems.CRAB_CAKES.get()); //Has crab in it so I will count it as a fish
-        manager.addFish(ModItems.CRAB_LEGS.get());
-        manager.addFish(ModItems.FISH_STICK.get());
-        manager.addPlant(ModItems.KELP_SHAKE.get(), 5); //This is a drink but it is made of kelp so plant
-        manager.addMeat(ModItems.RAW_CLAM_MEAT.get());
-        manager.addFish(ModItems.RAW_CLAWSTER.get());
-        manager.addFish(ModItems.RAW_CRAB.get());
-        manager.addFish(ModItems.RAW_SHRIMP.get());
-        manager.addMeat(ModItems.SEAFOOD_GUMBO.get()); //Dish made of meat, fish+plants, so I considered it a meat
-        manager.addFish(ModItems.SHRIMP_SKEWER.get());
-        manager.addFish(ModItems.STUFFED_NAUTILUS_SHELL.get());
-        manager.addMeat(ModItems.SURF_AND_TURF.get()); //Made of fish and meat, so meat
-        manager.addFish(ModItems.TROPICAL_FISH_SLICE.get());
+        manager.addFish(CDModItems.BISQUE.get()); //Dish made of fish+plants, so I considered it a fish
+        manager.addFish(CDModItems.CLAM_BAKE.get()); //Dish made of fish+plants, so I considered it a fish
+        manager.addMeat(CDModItems.CLAM_CHOWDER.get()); //Dish made of clam meat+plants, so I considered it a meat
+        manager.addMeat(CDModItems.COOKED_CLAM_MEAT.get()); //Clam meat so meat
+        manager.addFish(CDModItems.COOKED_CLAWSTER.get());
+        manager.addFish(CDModItems.COOKED_CRAB.get());
+        manager.addFish(CDModItems.COOKED_SHRIMP.get());
+        manager.addFish(CDModItems.COOKED_TROPICAL_FISH.get());
+        manager.addFish(CDModItems.COOKED_TROPICAL_FISH_SLICE.get());
+        manager.addFish(CDModItems.CRAB_CAKES.get()); //Has crab in it so I will count it as a fish
+        manager.addFish(CDModItems.CRAB_LEGS.get());
+        manager.addFish(CDModItems.FISH_STICK.get());
+        manager.addPlant(CDModItems.KELP_SHAKE.get(), 5); //This is a drink but it is made of kelp so plant
+        manager.addMeat(CDModItems.RAW_CLAM_MEAT.get());
+        manager.addFish(CDModItems.RAW_CLAWSTER.get());
+        manager.addFish(CDModItems.RAW_CRAB.get());
+        manager.addFish(CDModItems.RAW_SHRIMP.get());
+        manager.addMeat(CDModItems.SEAFOOD_GUMBO.get()); //Dish made of meat, fish+plants, so I considered it a meat
+        manager.addFish(CDModItems.SHRIMP_SKEWER.get());
+        manager.addFish(CDModItems.STUFFED_NAUTILUS_SHELL.get());
+        manager.addMeat(CDModItems.SURF_AND_TURF.get()); //Made of fish and meat, so meat
+        manager.addFish(CDModItems.TROPICAL_FISH_SLICE.get());
         //22/22 added
     }
 }
