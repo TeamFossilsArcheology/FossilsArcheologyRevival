4ca64332a244b73d85717a088b1a035259c1bd7f
DarkPred <26512194+DarkPred@users.noreply.github.com>
2025-09-11 18:47:11 +0200

    Remove duplicate render of bone helmet overlay

--- fabric/src/main/java/com/github/teamfossilsarcheology/fossil/fabric/FabricFossilModClient.java
+++ fabric/src/main/java/com/github/teamfossilsarcheology/fossil/fabric/FabricFossilModClient.java
@@ -3,21 +3,17 @@ package com.github.teamfossilsarcheology.fossil.fabric;
 import com.github.teamfossilsarcheology.fossil.FossilMod;
 import com.github.teamfossilsarcheology.fossil.block.ModBlocks;
 import com.github.teamfossilsarcheology.fossil.client.ClientInit;
-import com.github.teamfossilsarcheology.fossil.client.renderer.OverlayRenderer;
 import com.github.teamfossilsarcheology.fossil.fabric.client.model.PlantModelProvider;
 import com.github.teamfossilsarcheology.fossil.fabric.client.renderer.CustomItemRendererFabricImpl;
 import com.github.teamfossilsarcheology.fossil.fabric.client.renderer.armor.FabricAncientHelmetRenderer;
 import com.github.teamfossilsarcheology.fossil.item.ModItems;
 import com.github.teamfossilsarcheology.fossil.world.effect.ComfyBedEffect;
 import com.github.teamfossilsarcheology.fossil.world.effect.ModEffects;
-import com.mojang.blaze3d.platform.Window;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.model.ModelLoadingRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.BuiltinItemRendererRegistry;
-import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.fabricmc.fabric.api.entity.event.v1.EntitySleepEvents;
 import net.fabricmc.fabric.api.event.client.ClientSpriteRegistryCallback;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.texture.TextureAtlas;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.effect.MobEffectInstance;
@@ -39,10 +35,6 @@ public class FabricFossilModClient implements ClientModInitializer {
         ClientSpriteRegistryCallback.event(TextureAtlas.LOCATION_BLOCKS).register((atlasTexture, registry) -> {
             registry.register(FossilMod.location("block/tar_still"));
             registry.register(FossilMod.location("block/tar_flowing"));
-        });
-        HudRenderCallback.EVENT.register((matrixStack, tickDelta) -> {
-            Window window = Minecraft.getInstance().getWindow();
-            OverlayRenderer.renderHelmet(window.getGuiScaledWidth(), window.getGuiScaledHeight());
         });
         EntitySleepEvents.ALLOW_SLEEP_TIME.register((player, sleepingPos, vanillaResult) -> {
             if (ComfyBedEffect.canApply(Optional.of(sleepingPos), player.level)) {
