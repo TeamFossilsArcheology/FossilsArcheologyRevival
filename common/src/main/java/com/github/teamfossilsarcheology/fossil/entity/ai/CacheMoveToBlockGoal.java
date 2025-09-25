ba02eafa26aa471d74d1d8dddc6413f63b838df9
DarkPred <26512194+DarkPred@users.noreply.github.com>
2025-09-26 01:13:21 +0200

    Allow for more vertical distance to feeders

--- common/src/main/java/com/github/teamfossilsarcheology/fossil/entity/ai/CacheMoveToBlockGoal.java
+++ common/src/main/java/com/github/teamfossilsarcheology/fossil/entity/ai/CacheMoveToBlockGoal.java
@@ -4,6 +4,7 @@ import com.github.teamfossilsarcheology.fossil.entity.prehistoric.base.Prehistor
 import it.unimi.dsi.fastutil.longs.LongArrayList;
 import it.unimi.dsi.fastutil.longs.LongList;
 import net.minecraft.core.BlockPos;
+import net.minecraft.util.Mth;
 import net.minecraft.world.entity.ai.goal.Goal;
 import net.minecraft.world.entity.ai.goal.MoveToBlockGoal;
 import net.minecraft.world.level.LevelReader;
@@ -11,6 +12,7 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.pathfinder.Path;
 import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.Vec3;
 
 import java.util.Comparator;
 import java.util.EnumSet;
@@ -143,7 +145,7 @@ public abstract class CacheMoveToBlockGoal extends Goal {
     }
 
     protected void moveMobToBlock() {
-         entity.getNavigation().moveTo(path, calculateSpeedModifier());
+        entity.getNavigation().moveTo(path, calculateSpeedModifier());
     }
 
     public double acceptedDistance() {
@@ -214,7 +216,9 @@ public abstract class CacheMoveToBlockGoal extends Goal {
     }
 
     protected boolean checkReachedTarget() {
-        return targetPos.closerToCenterThan(entity.position(), acceptedDistance());
+        double horizontal = Vec3.atBottomCenterOf(targetPos).subtract(entity.position()).horizontalDistanceSqr();
+        double vertical = Mth.square(targetPos.getY() - entity.getY());
+        return horizontal < Mth.square(acceptedDistance()) && vertical < Mth.square(entity.getBbHeight() + 0.1);
     }
 
     protected boolean isReachedTarget() {
