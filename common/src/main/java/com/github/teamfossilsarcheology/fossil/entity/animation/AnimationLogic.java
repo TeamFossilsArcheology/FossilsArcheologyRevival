92c0c50775c0b0b4072c91732725735544aed4c6
larissa-fr <lixtwoeq@gmail.com>
2025-10-27 08:21:35 -0300

    fixed flying mob walking on air bug

--- common/src/main/java/com/github/teamfossilsarcheology/fossil/entity/animation/AnimationLogic.java
+++ common/src/main/java/com/github/teamfossilsarcheology/fossil/entity/animation/AnimationLogic.java
@@ -493,7 +493,7 @@ public class AnimationLogic<T extends Mob & PrehistoricAnimatable<T>> {
                 if (info != null) {
                     additionalLogic.put(info, entity::isOnGround);
                 }
-            } else if (!entity.isOnGround() && !event.getAnimatable().isFlying() && (entity.getY() - entity.yo) < -0.05) {
+            } else if (!entity.isOnGround()) {
                 addActiveAnimation(controller.getName(), AnimationCategory.FLY);
                 controller.transitionLengthTicks = 10;
                 animationSpeed = 0.5;
