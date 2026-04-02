de5bc63856316b2b338f8003a6806175cf275485
DarkPred <26512194+DarkPred@users.noreply.github.com>
2026-04-03 01:03:08 +0200

    Fix moving animations not playing for other players while riding

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,7 @@
 - A typo in the stegosaurus dinopedia bio
 - Crash when dinos get hurt while attack riding some mobs
 - Added correct sound to the dinopedia page button
+- Walking animations not playing in multiplayer when a mob is being ridden
 
 ### Mod Compatibility
 - Fixed crash when Ender's Delight is installed
--- fabric/src/main/java/com/github/teamfossilsarcheology/fossil/fabric/mixin/GeoEntityRendererMixin.java
+++ fabric/src/main/java/com/github/teamfossilsarcheology/fossil/fabric/mixin/GeoEntityRendererMixin.java
@@ -1,8 +1,12 @@
 package com.github.teamfossilsarcheology.fossil.fabric.mixin;
 
+import com.github.teamfossilsarcheology.fossil.entity.prehistoric.base.PrehistoricAnimatable;
 import com.github.teamfossilsarcheology.fossil.entity.prehistoric.base.PrehistoricLeaping;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -21,5 +25,14 @@ public abstract class GeoEntityRendererMixin {
             return false;
         }
         return original;
+    }
+
+    @WrapOperation(method = "actuallyRender(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/world/entity/Entity;Lsoftware/bernie/geckolib/cache/object/BakedGeoModel;Lnet/minecraft/client/renderer/RenderType;Lnet/minecraft/client/renderer/MultiBufferSource;Lcom/mojang/blaze3d/vertex/VertexConsumer;ZFIIFFFF)V",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;getDeltaMovement()Lnet/minecraft/world/phys/Vec3;"))
+    public Vec3 test(Entity entity, Operation<Vec3> original) {
+        if (entity instanceof PrehistoricAnimatable<?> && entity.isVehicle() && !entity.isControlledByLocalInstance()) {
+            return entity.position().subtract(new Vec3(entity.xOld, entity.yOld, entity.zOld));
+        }
+        return original.call(entity);
     }
 }
