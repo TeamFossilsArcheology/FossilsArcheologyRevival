3ce51d6368497f64293aa9e4b6f0f7b90f202e59
Catnip <26512194+DarkPred@users.noreply.github.com>
2026-06-02 22:50:55 +0200

    Fix anu boss music sometimes playing multiple times

--- common/src/main/java/com/github/teamfossilsarcheology/fossil/network/S2CMusicMessage.java
+++ common/src/main/java/com/github/teamfossilsarcheology/fossil/network/S2CMusicMessage.java
@@ -1,11 +1,15 @@
 package com.github.teamfossilsarcheology.fossil.network;
 
+import com.github.teamfossilsarcheology.fossil.sounds.ModMusic;
+import com.github.teamfossilsarcheology.fossil.sounds.ModSounds;
 import com.github.teamfossilsarcheology.fossil.sounds.MusicHandler;
 import dev.architectury.networking.NetworkChannel;
 import dev.architectury.networking.NetworkManager;
 import dev.architectury.utils.Env;
 import net.minecraft.core.Registry;
 import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.sounds.Music;
+import net.minecraft.sounds.Musics;
 import net.minecraft.sounds.SoundEvent;
 
 import java.util.function.Supplier;
@@ -30,10 +34,19 @@ public class S2CMusicMessage {
 
     private void apply(Supplier<NetworkManager.PacketContext> contextSupplier) {
         if (contextSupplier.get().getEnvironment() == Env.SERVER) return;
-        contextSupplier.get().queue(() -> MusicHandler.startMusic(soundEvent));
+        contextSupplier.get().queue(() -> MusicHandler.startMusic(getMusic(soundEvent)));
     }
 
     public static void register(NetworkChannel channel) {
         channel.register(S2CMusicMessage.class, S2CMusicMessage::write, S2CMusicMessage::new, S2CMusicMessage::apply);
+    }
+
+    private static Music getMusic(SoundEvent soundEvent) {
+        if (soundEvent == ModSounds.MUSIC_ANU.get()) {
+            return ModMusic.ANU;
+        } else if (soundEvent == ModSounds.MUSIC_FIRST_DINOSAUR.get()) {
+            return ModMusic.FIRST_DINOSAUR;
+        }
+        return Musics.GAME;
     }
 }
---
+++ common/src/main/java/com/github/teamfossilsarcheology/fossil/sounds/ModMusic.java
@@ -0,0 +1,8 @@
+package com.github.teamfossilsarcheology.fossil.sounds;
+
+import net.minecraft.sounds.Music;
+
+public class ModMusic {
+    public static final Music ANU = new Music(ModSounds.MUSIC_ANU.get(), 0, 0, true);
+    public static final Music FIRST_DINOSAUR = new Music(ModSounds.MUSIC_FIRST_DINOSAUR.get(), 0, 0, true);
+}
