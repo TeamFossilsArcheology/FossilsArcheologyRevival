78929a940cf2ddc493b57012ccb3d0ade73ac937
Catnip <26512194+DarkPred@users.noreply.github.com>
2026-06-02 02:51:15 +0200

    Fix jei display for recipes with "nothing" output

--- common/src/main/java/com/github/teamfossilsarcheology/fossil/compat/jei/MultiOutputCategory.java
+++ common/src/main/java/com/github/teamfossilsarcheology/fossil/compat/jei/MultiOutputCategory.java
@@ -82,12 +82,17 @@ public abstract class MultiOutputCategory<T extends MultiOutputAndSlotsRecipe> i
         List<IRecipeSlotView> slots = recipeSlotsView.getSlotViews(RecipeIngredientRole.OUTPUT);
         for (int i = 0; i < slots.size(); i++) {
             Optional<ItemStack> currentStack = slots.get(i).getDisplayedItemStack();
+            var slotItem = slotItems.get(i);
             if (currentStack.isEmpty()) {
-                break;
+                //Empty = Air = Nothing which is a valid output. We just need to check if there is a probability for nothing
+                slotItem.items.stream().filter(weightedItem -> weightedItem.stack.isEmpty()).findFirst().ifPresent(weightedItem -> {
+                    renderProbability(poseStack, Minecraft.getInstance().font, weightedItem.probability, slotItem.x, slotItem.y);
+                });
+                continue;
             }
-            for (WeightedItem item : slotItems.get(i).items) {
+            for (WeightedItem item : slotItem.items) {
                 if (ItemStack.isSame(currentStack.get(), item.stack)) {
-                    renderProbability(poseStack, Minecraft.getInstance().font, item.probability, slotItems.get(i).x, slotItems.get(i).y);
+                    renderProbability(poseStack, Minecraft.getInstance().font, item.probability, slotItem.x, slotItem.y);
                     break;
                 }
             }
